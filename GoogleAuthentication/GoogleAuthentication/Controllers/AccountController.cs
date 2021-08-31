94bec2e36fb66c4224e49cfb0a4919d7ce3a060f
LongNguyen <long.nguyendh@saigontechnology.com>
2021-08-31 22:45:43 +0700

    Google authentication

---
+++ GoogleAuthentication/GoogleAuthentication/Controllers/AccountController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.Google;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GoogleAuthentication.Controllers
+{
+    [AllowAnonymous, Route("account")]
+    public class AccountController : Controller
+    {
+        [Route("signin-google")]
+        public IActionResult GoogleLogin()
+        {
+            var properties = new AuthenticationProperties { RedirectUri = Url.Action("GoogleResponse") };
+
+            return Challenge(properties, GoogleDefaults.AuthenticationScheme);
+        }
+
+        [Route("google-response")]
+        public async Task<IActionResult> GoogleResponse()
+        {
+            var result = await HttpContext.AuthenticateAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+            var calims = result.Principal.Identities.FirstOrDefault()
+                .Claims.Select(x => new
+                {
+                    x.Issuer,
+                    x.OriginalIssuer,
+                    x.Type,
+                    x.Value
+                });
+            return Json(calims);
+        }
+    }
+}
